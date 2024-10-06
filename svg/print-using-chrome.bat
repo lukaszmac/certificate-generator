fb6c2a4736dfa8405693efefb94f7510cd89ae6a
Lukasz Machowski <lukaszmac@mweb.co.za>
2024-10-06 16:24:59 +0200

    Added example of using chrome to print from SVG to PDF.

---
+++ svg/print-using-chrome.bat
@@ -0,0 +1,14 @@
+REM https://developer.chrome.com/docs/chromium/headless
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "file:///C:/Mach/Source/certificate-generator/Template.svg"
+
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --print-to-pdf="C:\Mach\Source\certificate-generator\output.pdf" "file:///C:/Mach/Source/certificate-generator/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --no-pdf-header-footer --print-to-pdf="C:\Mach\Source\certificate-generator\output.pdf" "file:///C:/Mach/Source/certificate-generator/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --no-pdf-header-footer -print-to-pdf --timeout=5000 "file:///C:/Mach/Source/certificate-generator/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --no-pdf-header-footer -print-to-pdf --user-data-dir=C:\Mach\Source\certificate-generator\svg --data-path-dir=C:\Mach\Source\certificate-generator\svg --timeout=2000 "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --trace-to-console --headless --no-pdf-header-footer -print-to-pdf --timeout=2000 --data-path=C:\Mach\Source\certificate-generator\svg "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --no-pdf-header-footer -print-to-pdf --timeout=2000 --data-path=C:\Mach\Source\certificate-generator\svg "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --no-pdf-header-footer -print-to-pdf --timeout=2000 "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --no-pdf-header-footer --print-to-pdf="C:\Mach\Source\certificate-generator\svg\output.pdf" --timeout=2000 "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --print-to-pdf="C:\Mach\Source\certificate-generator\svg\output.pdf" "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+REM "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless -no-pdf-header-footer --print-to-pdf="C:\Mach\Source\certificate-generator\svg\output.pdf" "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --headless --no-pdf-header-footer --print-to-pdf="C:\Mach\Source\certificate-generator\svg\output.pdf" "file:///C:/Mach/Source/certificate-generator/svg/Template.svg"
